15cd75900b3a24ebc64bcedd23d74c9d9f540c11
EXT84173 <matias.orcajo@claro.com.ar>
2023-03-25 23:50:38 -0300

    Push

--- database/migrations/2023_03_25_204345_create_estudiantes_resumen_table.php
+++ database/migrations/2023_03_25_204345_create_estudiantes_resumen_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class EstudiantesResumen extends Migration
+class CreateEstudiantesResumenTable extends Migration
 {
     /**
      * Run the migrations.
