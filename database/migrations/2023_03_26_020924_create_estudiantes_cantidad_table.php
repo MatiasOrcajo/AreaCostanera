b8a59c629da615d8280dcffbe1263aa04abb1784
EXT84173 <matias.orcajo@claro.com.ar>
2023-03-25 23:51:10 -0300

    Push

--- database/migrations/2023_03_26_020924_create_estudiantes_cantidad_table.php
+++ database/migrations/2023_03_26_020924_create_estudiantes_cantidad_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class EstudiantesCantidad extends Migration
+class CreateEstudiantesCantidadTable extends Migration
 {
     /**
      * Run the migrations.
